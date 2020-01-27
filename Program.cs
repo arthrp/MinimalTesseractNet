3afe9bb1a5bfd602e03820c7836c128122eb3554
Arthur <arthrp@users.noreply.github.com>
2020-01-27 23:22:42 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Tesseract;
+
+namespace MinimalTesseractNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = args[0];
+            using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+            using (var img = Pix.LoadFromFile(path))
+            using (var page = engine.Process(img))
+            {
+                var text = page.GetText();
+                Console.WriteLine("Mean confidence: {0}", page.GetMeanConfidence());
+                Console.WriteLine($"Text: \r\n{text}");
+            }
+        }
+    }
+}
